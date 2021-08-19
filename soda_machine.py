9c91fec319ee59f6726d3a95700743f1b4b38905
TKanter92 <TylerKanter@yahoo.com>
2021-08-18 20:58:00 -0500

    changed line 48 in soda_machine.py to read (selected_soda) instead of (selected_soda_name). This allows the object to pass through, as opposed to just the string of the soda name. This allowed for our user's wallet amount to be implemented. Entire program now runs as it should. MVP achieved

--- soda_machine.py
+++ soda_machine.py
@@ -45,7 +45,7 @@ class SodaMachine:
         selected_soda = self.get_inventory_soda(selected_soda_name)
 
         customer_payment = customer.gather_coins_from_wallet(selected_soda)
-        self.calculate_transaction(customer_payment, selected_soda_name, customer)
+        self.calculate_transaction(customer_payment, selected_soda, customer)
 
         user_interface.output_text("Transaction complete")
 
